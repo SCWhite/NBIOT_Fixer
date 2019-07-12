90da592618d107ef2462a01a4c1c48211b1c8a10
SCWhite <sean3381212@gmail.com>
2019-07-13 00:01:57 +0800

    fix port

--- NBIOT_Fixer.py
+++ NBIOT_Fixer.py
@@ -4,30 +4,30 @@ import serial
 ser=serial.Serial("/dev/ttyUSB0",baudrate=115200, timeout=1)
 
 
-port.write("AT\r".encode())
+ser.write("AT\r".encode())
 data = ser.readline().decode()
 time.sleep(1)
 
-port.write("AT+IPR=57600\r".encode())
+ser.write("AT+IPR=57600\r".encode())
 data = ser.readline().decode()
 time.sleep(1)
 
-port.close()
+ser.close()
 
 print("OK close port")
 
 
 ser=serial.Serial("/dev/ttyUSB0",baudrate=57600, timeout=1)
 
-port.write("AT\r".encode())
+ser.write("AT\r".encode())
 data = ser.readline().decode()
 time.sleep(1)
 
-port.write("AT+IPR?\r".encode())
+ser.write("AT+IPR?\r".encode())
 data = ser.readline().decode()
 data = ser.readline().decode()
 data = ser.readline().decode()
 time.sleep(1)
 
 print("looks good")
-port.close()
+ser.close()
