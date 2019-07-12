c5199a81859e7c92e06628a3c00143e5cf5d7235
SCWhite <sean3381212@gmail.com>
2019-07-13 00:05:34 +0800

    print data

--- NBIOT_Fixer.py
+++ NBIOT_Fixer.py
@@ -6,10 +6,12 @@ ser=serial.Serial("/dev/ttyUSB0",baudrate=115200, timeout=1)
 
 ser.write("AT\r".encode())
 data = ser.readline().decode()
+print(data)
 time.sleep(1)
 
 ser.write("AT+IPR=57600\r".encode())
 data = ser.readline().decode()
+print(data)
 time.sleep(1)
 
 ser.close()
@@ -21,12 +23,16 @@ ser=serial.Serial("/dev/ttyUSB0",baudrate=57600, timeout=1)
 
 ser.write("AT\r".encode())
 data = ser.readline().decode()
+print(data)
 time.sleep(1)
 
 ser.write("AT+IPR?\r".encode())
 data = ser.readline().decode()
+print(data)
 data = ser.readline().decode()
+print(data)
 data = ser.readline().decode()
+print(data)
 time.sleep(1)
 
 print("looks good")
