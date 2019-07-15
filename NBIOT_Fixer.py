27559c28fde6bd1d757e3f56ad92302f089db4bc
SCWhite <sean3381212@gmail.com>
2019-07-15 12:31:58 +0800

    add try and except

--- NBIOT_Fixer.py
+++ NBIOT_Fixer.py
@@ -1,39 +1,48 @@
 import time
 import serial
 
-ser=serial.Serial("/dev/ttyUSB0",baudrate=115200, timeout=1)
 
+try:
+	ser=serial.Serial("/dev/ttyUSB0",baudrate=115200, timeout=1)
+except:
+	print "baudrate is not 115200 !!!"
 
-ser.write("AT\r".encode())
-data = ser.readline().decode()
-print(data)
-time.sleep(1)
+else:
+	ser.write("AT\r".encode())
+	data = ser.readline().decode()
+	print(data)
+	time.sleep(1)
 
-ser.write("AT+IPR=57600\r".encode())
-data = ser.readline().decode()
-print(data)
-time.sleep(1)
+	ser.write("AT+IPR=57600\r".encode())
+	data = ser.readline().decode()
+	print(data)
+	time.sleep(1)
 
-ser.close()
+finally:
+	ser.close()
 
-print("OK close port")
+	print("OK close port")
 
+try:
+	ser=serial.Serial("/dev/ttyUSB0",baudrate=57600, timeout=1)
+except:
+	print "baudrate is not 57600 !!!"
 
-ser=serial.Serial("/dev/ttyUSB0",baudrate=57600, timeout=1)
+else:
+	ser.write("AT\r".encode())
+	data = ser.readline().decode()
+	print(data)
+	time.sleep(1)
 
-ser.write("AT\r".encode())
-data = ser.readline().decode()
-print(data)
-time.sleep(1)
-
-ser.write("AT+IPR?\r".encode())
-data = ser.readline().decode()
-print(data)
-data = ser.readline().decode()
-print(data)
-data = ser.readline().decode()
-print(data)
-time.sleep(1)
-
-print("looks good")
-ser.close()
+	ser.write("AT+IPR?\r".encode())
+	data = ser.readline().decode()
+	print(data)
+	data = ser.readline().decode()
+	print(data)
+	data = ser.readline().decode()
+	print(data)
+	time.sleep(1)
+	
+finally:
+	print("looks good")
+	ser.close()
